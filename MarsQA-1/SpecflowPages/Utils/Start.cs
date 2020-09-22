06449c2c519a5eaf9799139222bdac832abe6786
ArchikaMehta <archika.mehta19@gmail.com>
2020-09-22 23:33:08 +1200

    Updated driver class as hooks for dependency injection

--- MarsQA-1/SpecflowPages/Utils/Start.cs
+++ MarsQA-1/SpecflowPages/Utils/Start.cs
@@ -1,5 +1,8 @@
-﻿using MarsQA_1.Helpers;
+﻿using BoDi;
+using MarsQA_1.Helpers;
 using MarsQA_1.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using RelevantCodes.ExtentReports;
 using System;
 using System.Collections.Generic;
@@ -14,36 +17,56 @@ using static MarsQA_1.Helpers.CommonMethods;
 namespace MarsQA_1.Utils
 {
     [Binding]
-    public class Start : Driver
+    public sealed class Start 
     {
+        //Initialize the browser
+        private IWebDriver _driver;
+        private readonly IObjectContainer _objectContainer;
+
+        public Start(IObjectContainer objectContainer)
+        {
+            _objectContainer = objectContainer;
+        }
 
         [BeforeScenario]
         public void Setup()
         {
             //launch the browser
             Initialize();
-            ExcelLibHelper.PopulateInCollection(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)+@"\SpecflowTests\Data\Mars.xlsx", "Credentials");
+            ExcelLibHelper.PopulateInCollection(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\SpecflowTests\Data\Mars.xlsx", "Credentials");
             //call the SignIn class
-            SignIn.SigninStep();
+            //SignIn.SigninStep();
+            ExtentReports();
+        }
+
+        private IWebDriver Initialize()
+        {
+            //Defining the browser   
+            _driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _objectContainer.RegisterInstanceAs(_driver);
+            //Maximise the window
+            _driver.Manage().Window.Maximize();
+
+            return _driver;
         }
 
         [AfterScenario]
         public void TearDown()
         {
-
             // Screenshot
-            string img = SaveScreenShotClass.SaveScreenshot(Driver.driver, "Report");
-           test.Log(LogStatus.Info, "Snapshot below: " + test.AddScreenCapture(img));
+            string img = SaveScreenShotClass.SaveScreenshot(_driver, "Report");            
+            test.Log(LogStatus.Info, "Snapshot below: " + test.AddScreenCapture(img));
+
             //Close the browser
-            Close();
+            _driver.Close();
+            _driver.Dispose();
              
             // end test. (Reports)
             CommonMethods.Extent.EndTest(test);
             
             // calling Flush writes everything to the log file (Reports)
             CommonMethods.Extent.Flush();
-           
-
         }
     }
 }
