ab055d30e6079896243eacdbe1dd2f4c52d0e559
ArchikaMehta <archika.mehta19@gmail.com>
2020-09-22 23:31:41 +1200

    Used updated driver name for already written steps

--- MarsQA-1/SpecflowPages/Pages/SignIn.cs
+++ MarsQA-1/SpecflowPages/Pages/SignIn.cs
@@ -1,39 +1,51 @@
-﻿using MarsQA_1.Helpers;
+﻿using NUnit.Framework;
+using MarsQA_1.Helpers;
+using MarsQA_1.Utils;
 using OpenQA.Selenium;
 using System.Threading;
 
 namespace MarsQA_1.Pages
 {
-    public static class SignIn
-    {
-        private static IWebElement SignInBtn =>  Driver.driver.FindElement(By.XPath("//A[@class='item'][text()='Sign In']"));
-        private static IWebElement Email => Driver.driver.FindElement(By.XPath("(//INPUT[@type='text'])[2]"));
-        private static IWebElement Password => Driver.driver.FindElement(By.XPath("//INPUT[@type='password']"));
-        private static IWebElement LoginBtn => Driver.driver.FindElement(By.XPath("//BUTTON[@class='fluid ui teal button'][text()='Login']"));
-        public static void SigninStep()
+    internal class SignIn :Driver
+    { 
+        public SignIn(IWebDriver driver) : base(driver)
         {
-            Driver.NavigateUrl();
+            
+        }
+        //Web element for Url
+        private IWebElement SignInBtn => _driver.FindElement(By.XPath("//A[@class='item'][text()='Sign In']"));
+        //Web element for Username
+        private IWebElement Email => _driver.FindElement(By.XPath("(//INPUT[@type='text'])[2]"));
+        //Web element for password
+        private IWebElement Password => _driver.FindElement(By.XPath("//INPUT[@type='password']"));
+        //Web element for Login Button
+        private IWebElement LoginBtn => _driver.FindElement(By.XPath("//BUTTON[@class='fluid ui teal button'][text()='Login']"));
+        
+        internal void SigninStep()
+        {
+            //Enter Url
             SignInBtn.Click();
+            //Enter Username
             Email.SendKeys(ExcelLibHelper.ReadData(2,"username"));
+            //Enter password
             Password.SendKeys(ExcelLibHelper.ReadData(2, "password"));
+            //Click on Login Button
             LoginBtn.Click();
         }
-        public static void Login()
-        {
-            Driver.NavigateUrl();
 
+        internal void Login()
+        {
             //Enter Url
-            Driver.driver.FindElement(By.XPath("//A[@class='item'][text()='Sign In']")).Click();
+            _driver.FindElement(By.XPath("//A[@class='item'][text()='Sign In']")).Click();
 
             //Enter Username
-            Driver.driver.FindElement(By.XPath("(//INPUT[@type='text'])[2]")).SendKeys("");
+            _driver.FindElement(By.XPath("(//INPUT[@type='text'])[2]")).SendKeys("");
 
             //Enter password
-            Driver.driver.FindElement(By.XPath("//INPUT[@type='password']")).SendKeys("");
+            _driver.FindElement(By.XPath("//INPUT[@type='password']")).SendKeys("");
 
             //Click on Login Button
-            Driver.driver.FindElement(By.XPath("//BUTTON[@class='fluid ui teal button'][text()='Login']")).Click();
-
+            _driver.FindElement(By.XPath("//BUTTON[@class='fluid ui teal button'][text()='Login']")).Click();
         }
     }
 }
