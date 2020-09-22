4ff7026e947bbef9a3677a84fa59bceb051e67f1
ArchikaMehta <archika.mehta19@gmail.com>
2020-09-22 23:43:21 +1200

    Created step definitions for login feature file

---
+++ MarsQA-1/StepDefinitions/LoginSD.cs
@@ -0,0 +1,37 @@
+﻿using MarsQA_1.Helpers;
+using MarsQA_1.Pages;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace MarsQA_1.Feature
+{
+    [Binding]
+    class LoginSD 
+    {
+        private SignIn LoginObj;
+
+        //Created constructor for dependency injection
+        public LoginSD(IWebDriver driver)
+        {
+            LoginObj = new SignIn(driver);
+        }
+        [Given(@"I navigate to the application portal")]
+        public void GivenINavigateToTheApplicationPortal()
+        {
+            LoginObj.NavigateUrl();
+        }
+
+        [When(@"I login using valid credentials")]
+        public void WhenILoginUsingValidCredentials()
+        {
+            LoginObj.SigninStep();
+        }
+
+
+    }
+}
