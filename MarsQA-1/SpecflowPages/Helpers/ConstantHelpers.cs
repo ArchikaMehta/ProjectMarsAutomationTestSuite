f8090367234025dc7a5215e144a002cd11a15af4
ArchikaMehta <archika.mehta19@gmail.com>
2020-09-23 00:22:35 +1200

    updated path variables to use dynamic path

--- MarsQA-1/SpecflowPages/Helpers/ConstantHelpers.cs
+++ MarsQA-1/SpecflowPages/Helpers/ConstantHelpers.cs
@@ -14,12 +14,15 @@ namespace MarsQA_1.Helpers
         public static string Url = "http://192.168.99.100:5000";
 
         //ScreenshotPath
-        public static string ScreenshotPath =  @"C:\Study\IndustryConnect\git\ProjectMarsAutomationTestSuite1\MarsQA-1\TestReports\Screenshots\";
+        //public static string ScreenshotPath =  @"C:\Study\IndustryConnect\git\ProjectMarsAutomationTestSuite1\MarsQA-1\TestReports\Screenshots\";
+        public static string ScreenshotPath = (Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"../../../\TestReports\Screenshots\");
 
         //ExtentReportsPath
-        public static string ReportsPath = @"C:\Study\IndustryConnect\git\ProjectMarsAutomationTestSuite1\MarsQA-1\TestReports\Report\MarsQA_AutomationTestReport.html";
+       // public static string ReportsPath = @"C:\Study\IndustryConnect\git\ProjectMarsAutomationTestSuite1\MarsQA-1\TestReports\Report\MarsQA_AutomationTestReport.html";
+        public static string ReportsPath = (Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"../../../\TestReports\Report\MarsQA_AutomationTestReport.html");
 
         //ReportXML Path
-        public static string ReportXMLPath = @"C:\Study\IndustryConnect\git\ProjectMarsAutomationTestSuite1\MarsQA-1\TestReports\Report\MarsQA_AutomationTestReport_Config.xml";
+        //public static string ReportXMLPath = @"C:\Study\IndustryConnect\git\ProjectMarsAutomationTestSuite1\MarsQA-1\TestReports\Report\MarsQA_AutomationTestReport_Config.xml";
+        public static string ReportXMLPath = (Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)+ @"../../../\TestReports\Report\MarsQA_AutomationTestReport_Config.xml");
     }
 }
